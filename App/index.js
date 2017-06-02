76a6426e51ebeceb62cdbcb1ea28cd287276ac63
Eric Tsai <eric.tsai@xuenn.com>
2017-06-02 16:14:50 +0800

    lab 2 study

---
+++ App/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/*
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                Hello, World.
+            </div>
+        )
+    }
+}
+*/
+
+const App = () => {
+    return (
+        <dev>
+            Hello, World.
+        </dev>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
